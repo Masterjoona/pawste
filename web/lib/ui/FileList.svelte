f13578f676586f0a8e0c26660cd3d2d89b887ec1
Masterjoona <69722179+Masterjoona@users.noreply.github.com>
2024-07-11 12:08:44 +0000

    insanity gone

--- web/lib/ui/FileList.svelte
+++ web/lib/ui/FileList.svelte
@@ -5,7 +5,7 @@
         prettifyFileSize,
         truncateFilename,
         viewFile,
-        isFileDb as isFromDb,
+        isFileDb as FileDatafromDb,
     } from "../utils";
 
     export let files: FileType[] = [];
@@ -16,36 +16,28 @@
 
 <div class="file-list">
     {#each files as file, index}
+        {@const dataFromDb = FileDatafromDb(file)}
         <div class="file-item">
-            {#if !isFromDb(file) && file.type.startsWith("image/")}
+            {#if !dataFromDb && file.type.startsWith("image/")}
                 <img
                     src={imageSources[index]}
                     alt={file.name}
                     class="thumbnail" />
             {/if}
-            {#if isFromDb(file)}
-                <span
-                    >{truncateFilename(file.Name)} - {prettifyFileSize(
-                        file.Size,
-                    )}</span>
-                {#if removeFile}
-                    <button on:click={() => removeFile(file.Name)}
-                        >Remove</button>
-                {/if}
-                <button on:click={() => viewFile(pasteName, file.Name)}
-                    >View</button>
-            {:else}
-                <span
-                    >{truncateFilename(file.name)} - {prettifyFileSize(
-                        file.size,
-                    )}</span>
-                {#if removeFile}
-                    <button on:click={() => removeFile(file.name)}
-                        >Remove</button>
-                {/if}
-                <button on:click={() => viewFile(pasteName, file.name)}
-                    >View</button>
+            <span>
+                {truncateFilename(dataFromDb ? file.Name : file.name)}
+                ({prettifyFileSize(dataFromDb ? file.Size : file.size)})
+            </span>
+            {#if removeFile}
+                <button
+                    on:click={() =>
+                        removeFile(dataFromDb ? file.Name : file.name)}
+                    >Remove</button>
             {/if}
+            <button
+                on:click={() =>
+                    viewFile(pasteName, dataFromDb ? file.Name : file.name)}
+                >View</button>
         </div>
     {/each}
 </div>
